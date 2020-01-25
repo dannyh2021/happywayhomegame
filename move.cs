75188e54d38ed7b9f9f37982974f95dcec7c47d6
Jeffligit <54647089+Jeffligit@users.noreply.github.com>
2020-01-25 16:52:34 -0500

    Update move.cs

--- move.cs
+++ move.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class move : MonoBehaviour
 {
+
+    public float startYPos;
+    public float startScale;
+
     // Start is called before the first frame update
     void Start()
     {
+        startYPos = transform.localPosition.y;
+        startScale = transform.localScale.y;
     }
 
     // Update is called once per frame
@@ -16,5 +22,6 @@ public class move : MonoBehaviour
         Quaternion q = transform.rotation;
         q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y, 0);
         transform.rotation = q;
+        transform.localScale = new Vector3 (startScale - (transform.localPosition.y / 4), startScale - (transform.localPosition.y / 4),0);
     }
 }
