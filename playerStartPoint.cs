7b12ddfa39449e1c77056cb8b8c02cdf2b43150d
Jeffligit <54647089+Jeffligit@users.noreply.github.com>
2020-02-15 13:45:23 -0500

    playerStartPoint when switching maps

---
+++ playerStartPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerStartPoint : MonoBehaviour
+{
+	private playerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<playerController>();
+        player.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
